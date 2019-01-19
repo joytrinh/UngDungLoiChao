534d82d0c5b85b82987181df465d8b730be35e08
Joy Trinh <mongtuyen149@gmail.com>
2019-01-19 23:06:24 +0700

    Tao loi chao khach tham quan va khach hang

---
+++ UngDungLoiChao/2.XuLy/XL_NGHIEPVU.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class XL_NGHIEPVU
+{
+    public static string TaoLoiChaoKhachThamQuan()
+    {
+        return "Xin chào khách tham quan";
+    }
+    public static string TaoLoiChaoKhachHang(string HoTen)
+    {
+        return "Xin chào Khách Hàng " + HoTen;
+    }
+    public static string TaoLoiChaoNhanVien(XL_NHANVIEN NV)
+    {
+        return "Xin chào Nhân viên " + NV.HoTen + "<br/>Số lần đăng nhập hiện nay là " 
+            + NV.DanhSachCacLanDangNhap.Count;
+    }
+    public static string TaoLoiChaoQuanLy(XL_QUANLY QL)
+    {
+        return "Xin chào Quản lý " + QL.HoTen + "<br/>Số lần đăng nhập hiện nay là "
+            + QL.DanhSachCacLanDangNhap.Count;
+    }
+}
