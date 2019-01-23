39b6e8f2218c146bb956a2ee8bed9eaaae060dbd
Joy Trinh <mongtuyen149@gmail.com>
2019-01-23 21:18:27 +0700

    Create MH_ChaoQuanLy

--- UngDungLoiChao/2.XuLy/XL_THEHIEN.cs
+++ UngDungLoiChao/2.XuLy/XL_THEHIEN.cs
@@ -35,6 +35,14 @@ public class XL_THEHIEN
             "Anh Chị đã được phân công " + NhanVien.DanhSachMaSoNhomHang.Count + " Nhóm hàng</div><br/>";
         return chuoiHTML;
     }
+    public static string TaoChuoiHTMLQuanLy(XL_QUANLY QuanLy)
+    {
+        string chuoiHinh = "<img src='../Media/" + QuanLy.MaSo + ".png' width='150' height='150'> <br/>";
+        string chuoiThongTin = XL_NGHIEPVU.TaoLoiChaoQuanLy(QuanLy);
+        string chuoiHTML = "<div class='btn'>" + $"{chuoiHinh}" + $"{chuoiThongTin}" +
+            "<br/>Đây là Ứng dụng Web<b> dành riêng cho Xử lý thể hiện</b> của tôi </div><br/>";
+        return chuoiHTML;
+    }
 
     //Tao chuoi HTML Danh sach doi tuong
     public static string TaoChuoiHTMLDanhSachNhomHang(List<XL_NHOMHANG> DanhSachNhomHang)
@@ -49,14 +57,35 @@ public class XL_THEHIEN
         });
         return TieuDe + chuoiHTML;
     }
-    public static string TaoChuoiHTMLCacLanDangNhap(XL_NHANVIEN NhanVien)
+    public static string TaoChuoiHTMLDanhSachNhanVien(List<XL_NHANVIEN> DanhSachNhanVien)
+    {
+        string TieuDe = $"<div class='btn'> Danh sách {DanhSachNhanVien.Count} Nhân viên<div><br/>";
+        string chuoiHTML = "<div class='btn'>";
+        DanhSachNhanVien.ForEach(NhanVien =>
+        {
+            string chuoiHinh = "<div class='btn'> <img src='../Media/" + NhanVien.MaSo + ".png' width='50' height='50'>";
+            string chuoiThongTin = "<br/><div>" + NhanVien.HoTen + "</div>";
+            chuoiHTML += $"{chuoiHinh}" + $"{chuoiThongTin}" + "<br />";
+        });
+        return TieuDe + chuoiHTML;
+    }
+    public static string TaoChuoiHTMLCacLanDangNhap(List<DateTime> DanhSachCacLanDangNhap)
     {
         string chuoiHTML_CacLanDangNhap = "<br/><div>Danh sách các lần đăng nhập";
-        NhanVien.DanhSachCacLanDangNhap.ForEach(ngay =>
+        DanhSachCacLanDangNhap.ForEach(ngay =>
         {
-            chuoiHTML_CacLanDangNhap += "<br/>" + ngay.ToString();
+            chuoiHTML_CacLanDangNhap += "<br/>" + ngay.ToString(DinhDangVN);
         });
         chuoiHTML_CacLanDangNhap += "</div>";
         return chuoiHTML_CacLanDangNhap;
+    }
+
+    //Tao chuoi HTML thong bao yeu cau dang nhap
+    public static string TaoChuoiHTMLThongBaoYeuCauDangNhap()
+    {
+        string chuoiHTML = "<div>" + "Xin vui lòng Nhập Tên đăng nhập và Mật khẩu"
+            + "<br/> Click đồng ý => Nếu hợp lệ Ứng dụng sẽ chào anh/chị"
+            + "<div>";
+        return chuoiHTML;
     }
 }
